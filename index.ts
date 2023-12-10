c28bc2ef4376cf501ccc8560b7f0dcc234fecc9b
Leandro-Calderon <mellyleo17@gmail.com>
2023-12-09 23:20:09 -0300

    typescript ejercicio de bandas de musica

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
-// crear la clase Banda aquí
+class Album {
+  title: string;
+  songs: string[];
+}
 
 class Banda {
   members: string[];
   albums: any[];
-  constructor(members: string[], albums: any[]) {
+  constructor(members: string[], albums: Album[]) {
     this.members = members;
     this.albums = albums;
   }
